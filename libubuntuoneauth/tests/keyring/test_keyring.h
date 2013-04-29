39ae0637317908704bde894a9aeee449140b6917
Rodney Dawes <rodney.dawes@canonical.com>
2013-04-29 17:49:20 -0400

    Add a test for getTokenName and the beginning of a place for keyring tests.

---
+++ libubuntuoneauth/tests/keyring/test_keyring.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef _TEST_KEYRING_H_
+#define _TEST_KEYRING_H_
+
+#include <QtTest/QtTest>
+
+class TestKeyring: public QObject
+{
+    Q_OBJECT
+ public:
+    TestKeyring();
+
+ private:
+    QString test_token_name, test_hostname;
+
+ private slots:
+    void testTokenName();
+};
+
+#endif /* _TEST_KEYRING_H_ */
+
