001f994632c11727d27f5d074da93c212e1ac10a
Rodney Dawes <rodney.dawes@canonical.com>
2013-05-24 11:38:25 -0400

    Delete the created tokens in the tests.

--- libubuntuoneauth/tests/test_token.cpp
+++ libubuntuoneauth/tests/test_token.cpp
@@ -34,12 +34,14 @@ void TestToken::testEmptyToken()
 {
     Token *token = new Token();
     QVERIFY(!token->isValid());
+    delete token;
 }
 
 void TestToken::testTokenArgs()
 {
     Token *token = new Token("a", "b", "c", "d");
     QVERIFY(token->isValid());
+    delete token;
 }
 
 void TestToken::testTokenCopy()
@@ -47,6 +49,7 @@ void TestToken::testTokenCopy()
     Token *old_token = new Token("a", "b", "c", "d");
     Token new_token = Token(*old_token);
     QVERIFY(new_token.isValid());
+    delete old_token;
 }
 
 void TestToken::testToQuery()
@@ -55,12 +58,14 @@ void TestToken::testToQuery()
     QString query = token->toQuery();
     QString expected("consumer_key=consumer&consumer_secret=c_secret&name=Ubuntu+One+%40+" + this->test_hostname + "&token=token&token_secret=t_secret");
     QCOMPARE(query.toUtf8().data(), expected.toUtf8().data());
+    delete token;
 }
 
 void TestToken::testFromQuery()
 {
     Token *token = Token::fromQuery("consumer_key=c&consumer_secret=c_s&name=Name+%40+hostname&token=t&token_secret=t_s");
     QVERIFY(token->isValid());
+    delete token;
 }
 
 void TestToken::testTokenName()
