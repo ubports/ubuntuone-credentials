94d0f6b3e8327d0faa65d3b9cb33c81bb78e884e
Michael McCracken <mike.mccracken@canonical.com>
2013-08-19 10:47:11 -0700

    add .provider and -sharing.service config files, and change put-on-device to copy the provider over.

--- online-accounts-provider/put-on-device.sh
+++ online-accounts-provider/put-on-device.sh
@@ -1,6 +1,9 @@
+#! /bin/sh
 
+SSH_DO="ssh -t -p 2222 phablet@127.0.0.1"
 
-scp -P 2222 *.qml phablet@127.0.0.1:/home/phablet/provider-temp
+scp -P 2222 *.qml *.provider phablet@127.0.0.1:/home/phablet/provider-temp
 
-ssh -t -p 2222 phablet@127.0.0.1 sudo cp /home/phablet/provider-temp/*.qml /usr/share/accounts/qml-plugins/ubuntuone/
-ssh -t -p 2222 phablet@127.0.0.1 ls -l --color /usr/share/accounts/qml-plugins/ubuntuone/
+$SSH_DO sudo cp /home/phablet/provider-temp/*.qml /usr/share/accounts/qml-plugins/ubuntuone/
+$SSH_DO sudo cp /home/phablet/provider-temp/ubuntuone.provider /usr/share/accounts/providers/ubuntuone.provider
+$SSH_DO ls -l --color /usr/share/accounts/qml-plugins/ubuntuone/
